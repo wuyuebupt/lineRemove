537733ddf97f6f7376d478c8d18ba60e553ae147
wuyuebupt <wuyuebupt@gmail.com>
2018-02-12 12:39:28 -0500

    a modified verison by Yue

--- edit_lists.py
+++ edit_lists.py
@@ -17,33 +17,40 @@ names_file = "name.en"
 
 with open(names_file) as f:
 	names = f.read().splitlines()
+print len(keys)
+print len(names)
 
 #print names
 
 print "The names have been read"
 
 #block of code that compares the keys
-for keyFromKey in keys:
-	keyFromName = []
-	indexOfKey = 0
-	length = len(keyFromKey)
-	while indexOfKey < len(names):
-		keyFromName.append(names[indexOfKey][0:length:])
-		indexOfKey += 1
-	if keyFromKey in keyFromName:
-		del names[keyFromName.index(keyFromKey)]
+if 0:
+	for keyFromKey in keys:
+		keyFromName = []
+		indexOfKey = 0
+		length = len(keyFromKey)
+		while indexOfKey < len(names):
+			keyFromName.append(names[indexOfKey][0:length:])
+			indexOfKey += 1
+		if keyFromKey in keyFromName:
+			del names[keyFromName.index(keyFromKey)]
 		#print "Key deleted"
 		#print names
 
+# block of code from Yue
+if 1:
+	output_file = open("output-yue",'w')
+	for index, nameFromNames in enumerate(names):
+		# print nameFromNames
+		arr = nameFromNames.strip().split()
+		keyofname = arr[0]
+		if keyofname not in keys:
+			output_file.write(nameFromNames)
+			output_file.write("\n")
+			
+	output_file.close()
+			
+
 print "The keys have been compared and deleted if found in both files "
-
-#overwrites the file to replace the names with the modified list
-output_file = open(names_file,'w')
-
-for name in names:
-	output_file.write(name)
-	output_file.write("\n")
-
-output_file.close()
-
 print "Names have been output to the file"
